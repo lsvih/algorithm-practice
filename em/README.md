caa6d165ea8108707ecf03e736dc4a44e96b4d78
lyz <408772145@qq.com>
2017-03-27 14:54:22 +0800

    Finished EM algorithm

--- em/README.md
+++ em/README.md
@@ -1,3 +1,14 @@
 [Article](http://lsvih.com/?p=539)
 
 [Derivation](http://lsvih.com/?p=515)
+
+Using numpy and matplotlib
+
+
+<p align="center">
+<img width="400" src="./init.png" text-align="center">
+</p>
+
+<p align="center">
+<img width="400" src="./result.png" text-align="center">
+</p>
--- em/em.py
+++ em/em.py
@@ -1,31 +1,43 @@
 ##############################
 #   Written By lsvih         #
-#   2017-03-23               #
+#   2017-03-27               #
 #   EM algorithm             #
 ##############################
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+mu1,mu2 = 40,20
+sigma = 6
+
 def generateDataAndShow():
-    mu1,mu2 = 50,120
-    sigma1,sigma2 = 20,30
-    x1,x2 = mu1 + sigma1 * np.random.randn(500),mu2 + sigma2 * np.random.randn(500)
-    plt.hist([x1,x2],50)
+    x1,x2 = mu1 + sigma * np.random.randn(500),mu2 + sigma * np.random.randn(500)
+    x = np.append(x1,x2)
+    plt.hist(x,50,normed=True)
     plt.show()
-    return x1+x2
-
-def eStep():
-
-def mStep():
+    return x
 
 if __name__=='__main__':
     data = generateDataAndShow()
-    mu = 70
+    Z = [[],[]]
     while True:
-        old_mu = mu
-        eStep()
-        mStep()
-        if abs(mu-old_mu) <= 0.001:
+        o_mu1,o_mu2 = mu1,mu2
+        Z = np.zeros([len(data),2])
+        #E-Step:
+        for i,X in enumerate(data):
+            p1 = np.exp(-(X-mu1)**2/(2*sigma**2))/(sigma*(np.sqrt(2*np.pi)))
+            p2 = np.exp(-(X-mu2)**2/(2*sigma**2))/(sigma*(np.sqrt(2*np.pi)))
+            Z[i,0],Z[i,1] = p1/(p1+p2),p2/(p1+p2)
+        #M-Step:
+        mu1,mu2 = np.dot(np.array(data),np.array(Z))/np.sum(Z,axis=0)
+        if abs(o_mu1-mu1)+abs(o_mu2-mu2) <= 0.001:
+            print mu1,mu2
             break
-    plt.hist([x1,x2],50)
+    #Darw Gaussian function
+    import matplotlib.mlab as mlab
+    fig, ax = plt.subplots()
+    n,bins,p = ax.hist(data,50,normed=True)
+    y1,y2 = mlab.normpdf(bins, mu1, sigma), mlab.normpdf(bins, mu2, sigma)
+    ax.plot(bins, y1/2)
+    ax.plot(bins, y2/2)
     plt.show()
